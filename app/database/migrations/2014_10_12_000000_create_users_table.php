46ff1da8c8fbeef1ffb185053363afba2809d9c7
Yuta-Iizuka <zunzun2682@gmail.com>
2022-10-07 22:43:29 +0900

    add user migration

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name',16);
+            $table->string('tel',16);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('division');
             $table->rememberToken();
             $table->timestamps();
         });
